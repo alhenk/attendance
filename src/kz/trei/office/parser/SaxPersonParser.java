d2af0300e8308a667136d4e4ad601886735b868a
alhenk <alexandr.g.koryagin@gmail.com>
2014-03-11 14:30:31 +0600

    developing SAX handler

--- src/kz/trei/office/parser/SaxPersonParser.java
+++ src/kz/trei/office/parser/SaxPersonParser.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import kz.trei.office.hr.Employee;
 import kz.trei.office.hr.Person;
 import kz.trei.office.rfid.RfidUID;
 import kz.trei.office.util.FileManager;
@@ -58,13 +59,15 @@ public class SaxPersonParser implements PersonParser {
 		RfidUID uid;
 		UUID uuid;
 
+		StringBuilder elementValue = new StringBuilder();
+
 		public PersonHandler() {
 		}
 
 		public void startElement(String uri, String localName, String qName,
 				Attributes attributes) throws SAXException {
-			// LOGGER.info("Start Element :" + qName);
-			if (qName.equalsIgnoreCase("Employee")) {
+			// LOGGER.debug("Start Element :" + qName);
+			if (qName.equalsIgnoreCase("EMPLOYEE")) {
 				String value = attributes.getValue("uuid");
 				if (value == null) {
 					uuid = UUID.randomUUID();
@@ -72,23 +75,23 @@ public class SaxPersonParser implements PersonParser {
 					uuid = UUID.fromString(value);
 				}
 				boolRfidTag = true;
-			} else if (qName.equalsIgnoreCase("firstName")) {
+			} else if (qName.equalsIgnoreCase("FIRSTNAME")) {
 				boolFirstName = true;
-			} else if (qName.equalsIgnoreCase("patronym")) {
+			} else if (qName.equalsIgnoreCase("PATRONYM")) {
 				boolPatronym = true;
-			} else if (qName.equalsIgnoreCase("LastName")) {
+			} else if (qName.equalsIgnoreCase("LASTNAME")) {
 				boolLastName = true;
-			} else if (qName.equalsIgnoreCase("birthday")) {
+			} else if (qName.equalsIgnoreCase("BIRTHDAY")) {
 				boolBirthday = true;
-			} else if (qName.equalsIgnoreCase("position")) {
+			} else if (qName.equalsIgnoreCase("POSITION")) {
 				boolPosition = true;
-			} else if (qName.equalsIgnoreCase("department")) {
+			} else if (qName.equalsIgnoreCase("DEPARTMENT")) {
 				boolDepartment = true;
-			} else if (qName.equalsIgnoreCase("room")) {
+			} else if (qName.equalsIgnoreCase("ROOM")) {
 				boolRoom = true;
-			} else if (qName.equalsIgnoreCase("tableId")) {
+			} else if (qName.equalsIgnoreCase("TABLEID")) {
 				boolTableId = true;
-			} else if (qName.equalsIgnoreCase("RfidTag")) {
+			} else if (qName.equalsIgnoreCase("RFIDTAG")) {
 				String value = attributes.getValue("uid");
 				if (value == null) {
 					uid = null;
@@ -96,29 +99,89 @@ public class SaxPersonParser implements PersonParser {
 					uid = RfidUID.createUID(value);
 				}
 				boolRfidTag = true;
-			} else if (qName.equalsIgnoreCase("tagType")) {
+			} else if (qName.equalsIgnoreCase("TAGTYPE")) {
 				boolTagType = true;
-			}else if (qName.equalsIgnoreCase("Issue")) {
+			} else if (qName.equalsIgnoreCase("ISSUE")) {
 				boolIssue = true;
-			}else if (qName.equalsIgnoreCase("issueDate")) {
+			} else if (qName.equalsIgnoreCase("ISSUEDATE")) {
 				boolIssueDate = true;
+			} else if (qName.equalsIgnoreCase("EXPIRATIONDATE")) {
+				boolExpirationDate = true;
 			}
 
 		}
 
 		public void endElement(String uri, String localName, String qName)
 				throws SAXException {
-			// LOGGER.info("End Element :" + qName);
+			// LOGGER.debug("End Element :" + qName);
+			if (qName.equalsIgnoreCase("EMPLOYEE")) {
+				LOGGER.debug(new Employee(uuid));
+			}
 		}
 
 		public void characters(char ch[], int start, int length) {
+			if (boolEmployee) {
+				LOGGER.debug("UUID = " + uuid);
+				boolEmployee = false;
+			} else if (boolFirstName) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("First Name = " + new String(ch, start, length));
+				boolFirstName = false;
+			} else
+
+			if (boolPatronym) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Patronym = " + new String(ch, start, length));
+				boolPatronym = false;
+			} else
+
 			if (boolLastName) {
-				LOGGER.info("Last Name = " + new String(ch, start, length));
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Last Name = " + new String(ch, start, length));
 				boolLastName = false;
-			}
-			if (boolRfidTag) {
-				LOGGER.info("UID = " + uid);
+			} else if (boolBirthday) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Birthday = " + new String(ch, start, length));
+				boolBirthday = false;
+			} else if (boolPosition) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Position = " + new String(ch, start, length));
+				boolPosition = false;
+			} else if (boolDepartment) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Department = " + new String(ch, start, length));
+				boolDepartment = false;
+			} else if (boolRoom) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Room = " + new String(ch, start, length));
+				boolRoom = false;
+			} else if (boolTableId) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Table ID = " + new String(ch, start, length));
+				boolTableId = false;
+			} else if (boolRfidTag) {
+				LOGGER.debug("UID = " + uid);
 				boolRfidTag = false;
+			} else if (boolTagType) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Tag type = " + new String(ch, start, length));
+				boolTagType = false;
+			} else if (boolProtocol) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Protocol = " + new String(ch, start, length));
+				boolProtocol = false;
+			} else if (boolIssue) {
+				LOGGER.debug("Issue ");
+				boolIssue = false;
+			} else if (boolIssueDate) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Issue date = " + new String(ch, start, length));
+				boolIssueDate = false;
+			} else if (boolExpirationDate) {
+				elementValue.append(new String(ch, start, length));
+				LOGGER.debug("Expiration date = "
+						+ new String(ch, start, length));
+				boolExpirationDate = false;
 			}
 		}
 	}
