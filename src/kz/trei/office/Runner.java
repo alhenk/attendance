549cb007c0be57518b5dc723b132c8f31c6dc41b
alhenk <alexandr.g.koryagin@gmail.com>
2014-03-11 19:02:23 +0600

    insignificant change

--- src/kz/trei/office/Runner.java
+++ src/kz/trei/office/Runner.java
@@ -2,35 +2,21 @@ package kz.trei.office;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
-import kz.trei.office.hr.Employee;
 import kz.trei.office.hr.Person;
+import kz.trei.office.parser.PersonParser;
 import kz.trei.office.parser.SaxPersonParser;
-import kz.trei.office.structure.Table1C;
 import kz.trei.office.util.FileManager;
 
 public class Runner {
 	private static final Logger LOGGER = Logger.getLogger(Runner.class);
 	
 	public static void main(String[] args) {
-		
-		
-		// LOGGER.info(FileManager.getResourceAsStream("staff.xml"));
-		LOGGER.info("UUID = " + UUID.randomUUID());
-		Person person = new Employee.Builder()
-				.setUUID("d0f3de3a-260a-480f-af56-4d9086de61aa")
-				.setLastName("Koryagin").build();
-		LOGGER.info(person);
-		
-		Table1C table = Table1C.createID("tt12345678");
-		LOGGER.info(table.getId());
-		table = Table1C.createRandomID();
-		LOGGER.info(table.getId());
-		SaxPersonParser parser = new SaxPersonParser();
 		List<Person> personnel = new ArrayList<Person>();
+
+		PersonParser parser = new SaxPersonParser();
 		personnel = parser.parse(FileManager.getResourceAsStream("staff.xml"));
 		LOGGER.info(personnel);
 	}
--- src/kz/trei/office/TestDrive.java
+++ src/kz/trei/office/TestDrive.java
@@ -3,10 +3,14 @@ package kz.trei.office;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 import org.apache.log4j.Logger;
 
+import kz.trei.office.hr.Employee;
+import kz.trei.office.hr.Person;
 import kz.trei.office.rfid.RfidUID;
+import kz.trei.office.structure.Table1C;
 
 
 
@@ -17,6 +21,19 @@ public class TestDrive {
 		//LOGGER.info(FileManager.readFile("office.properties"));
 		RfidUID uid = RfidUID.createUID("E0040100594737350000");
 		LOGGER.info("UID = " + uid.getValue());
+		
+		
+		// LOGGER.info(FileManager.getResourceAsStream("staff.xml"));
+		LOGGER.info("UUID = " + UUID.randomUUID());
+		Person person = new Employee.Builder()
+				.setUUID("d0f3de3a-260a-480f-af56-4d9086de61aa")
+				.setLastName("Koryagin").build();
+		LOGGER.info(person);
+		
+		Table1C table = Table1C.createID("tt12345678");
+		LOGGER.info(table.getId());
+		table = Table1C.createRandomID();
+		LOGGER.info(table.getId());
 		
 		SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd");
 		try {
