db64e8a0cb379ca2f7a5b5534852be33bd3450ac
alhenk <alexandr.g.koryagin@gmail.com>
2014-03-16 22:15:49 +0600

    DOM parser refactoring

--- src/kz/trei/office/Runner.java
+++ src/kz/trei/office/Runner.java
@@ -1,6 +1,8 @@
 package kz.trei.office;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -11,6 +13,18 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import kz.trei.office.hr.Employee;
+import kz.trei.office.hr.Person;
+import kz.trei.office.rfid.Issue;
+import kz.trei.office.rfid.ProtocolType;
+import kz.trei.office.rfid.RfidTag;
+import kz.trei.office.rfid.RfidType;
+import kz.trei.office.rfid.RfidUID;
+import kz.trei.office.structure.DepartmentType;
+import kz.trei.office.structure.PositionType;
+import kz.trei.office.structure.RoomType;
+import kz.trei.office.structure.Table1C;
+import kz.trei.office.util.DateStamp;
 import kz.trei.office.util.PropertyManager;
 
 public class Runner {
@@ -42,34 +56,75 @@ public class Runner {
 					.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			Document doc = dBuilder.parse(file);
-			//doc.getDocumentElement().normalize();
-
-			System.out.println("Root element :"
-					+ doc.getDocumentElement().getNodeName());
-			NodeList nList = doc.getElementsByTagName("tns:employee");
-			System.out.println("----------------------------");
-			System.out.println(nList.getLength());
-			for (int temp = 0; temp < nList.getLength(); temp++) {
-				Node nNode = nList.item(temp);
-				System.out.println("\nCurrent Element :" + nNode.getNodeName());
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					System.out.println("Staff id : "
-							+ eElement.getAttribute("tableId"));
-					System.out.println("First Name : "
-							+ eElement.getElementsByTagName("firstName")
+			doc.getDocumentElement().normalize();
+			Element root = doc.getDocumentElement();
+			System.out.println("Root element :" + root.getNodeName());
+			NodeList nodes = root.getElementsByTagName("tns:employee");
+			Employee.Builder employee;
+			RfidTag.Builder rfidTag;
+			List<Person> staff = new ArrayList<Person>();
+			for (int idx = 0; idx < nodes.getLength(); idx++) {
+				Node node = nodes.item(idx);
+				employee = new Employee.Builder();
+				System.out.println("\nCurrent Element :" + node.getNodeName());
+				if (node.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) node;
+					employee.setTableId(Table1C.createID(eElement.getAttribute("tableId")));
+					employee.setFirstName(eElement.getElementsByTagName("firstName")
 									.item(0).getTextContent());
+					String patronym = eElement.getElementsByTagName("patronym")
+							.item(0).getTextContent();
+					if (patronym != null) {
+						employee.setPatronym(patronym);
+					}
+					employee.setLastName(eElement.getElementsByTagName("lastName")
+							.item(0).getTextContent());
 					System.out.println("Last Name : "
 							+ eElement.getElementsByTagName("lastName").item(0)
 									.getTextContent());
-					System.out.println("Birthday : "
-							+ eElement.getElementsByTagName("birthday").item(0)
-									.getTextContent());
-
+					employee.setBirthday(DateStamp.create(eElement.getElementsByTagName("birthday")
+							.item(0).getTextContent()));
+					for (int i = 0; i < eElement.getElementsByTagName(
+							"position").getLength(); i++) {
+						employee.addPosition(PositionType.valueOf(eElement.getElementsByTagName("position")
+										.item(i).getTextContent()));
+					}
+					employee.setDepartment(DepartmentType.valueOf(eElement.getElementsByTagName("department")
+							.item(0).getTextContent()));
+					for (int i = 0; i < eElement.getElementsByTagName("room")
+							.getLength(); i++) {
+						RoomType roomType = RoomType.DEFAULT;
+						employee.addRoom( roomType.select(Integer.valueOf(eElement.getElementsByTagName("room").item(i)
+										.getTextContent())));
+					}
+					rfidTag = new RfidTag.Builder();
+					NodeList rfidNodes = eElement.getElementsByTagName("rfidTag");
+					Element rfidElement = (Element) rfidNodes.item(0);
+					rfidTag.setRfidUID(RfidUID.createUID(rfidElement.getAttribute("uid")));
+					rfidTag.setProtocol(ProtocolType.valueOf(rfidElement.getElementsByTagName("protocol")
+							.item(0).getTextContent()));
+					rfidTag.setRfidType(RfidType.valueOf(rfidElement.getElementsByTagName("tagtype")
+							.item(0).getTextContent()));
+					Issue.Builder issue = new Issue.Builder(); 
+					rfidNodes = eElement.getElementsByTagName("issue");
+					rfidElement = (Element) rfidNodes.item(0);
+					issue.setIssueDate(DateStamp.create(rfidElement.getElementsByTagName("issueDate")
+							.item(0).getTextContent()));
+					issue.setIssueDate(DateStamp.create(rfidElement
+							.getElementsByTagName("expirationDate").item(0)
+							.getTextContent()));
+					rfidTag.setIssue(issue.build());
+					employee.setTag(rfidTag.build());
+					staff.add(employee.build());
 				}
+			}
+			for (Person person : staff){
+				System.out.println((Employee)person);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		
 	}
 }
