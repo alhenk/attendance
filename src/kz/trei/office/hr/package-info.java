2fb8d4d3c000077e8afa88dfc0986b964f90eac8
alhenk <alexandr.g.koryagin@gmail.com>
2014-03-17 23:58:15 +0600

    added package-info.java

---
+++ src/kz/trei/office/hr/package-info.java
@@ -0,0 +1,6 @@
+@XmlSchema(
+namespace = "http://www.trei.kz/attendance/tns",
+elementFormDefault = XmlNsForm.QUALIFIED,
+xmlns={@XmlNs(prefix="tns", namespaceURI="http://www.trei.kz/attendance/tns")})  
+package kz.trei.office.hr;
+import javax.xml.bind.annotation.*;
